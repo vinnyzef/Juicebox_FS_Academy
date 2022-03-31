87b123b66d57bb7c7b5f108ed599a5637ea74031
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2022-03-30 20:28:36 -0500

    part 1 done

---
+++ db/index.js
@@ -0,0 +1,35 @@
+// inside db/index.js
+const { Client } = require("pg"); // imports the pg module
+
+// supply the db name and location of the database
+const client = new Client("postgres://localhost:5432/juicebox-dev");
+
+getAllUsers = async () => {
+  const { rows } = await client.query(` 
+  SELECT id, username FROM users;
+  `);
+  return rows;
+};
+async function createUser({ username, password }) {
+  try {
+    const {rows} = await client.query(
+      `
+        INSERT INTO users(username, password) 
+        VALUES($1, $2) 
+        ON CONFLICT (username) DO NOTHING 
+        RETURNING *;
+      `,
+      [username, password]
+    );
+
+    return rows;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+  client,
+  getAllUsers,
+  createUser,
+};
