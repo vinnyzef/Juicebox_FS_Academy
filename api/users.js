521ccbd8a0f22721cf381fb18ed558437b0513d9
vinnyzef <72477005+vinnyzef@users.noreply.github.com>
2022-04-10 15:06:57 -0500

    starting pt 3

--- api/users.js
+++ api/users.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const usersRouter = express.Router();
 const { getAllUsers } = require("../db");
+const { createUser } = require("../db");
 const { getUserByUsername } = require("../db");
 const jwt = require("jsonwebtoken");
 const { token } = require("morgan");
@@ -17,6 +18,45 @@ usersRouter.get("/", async (req, res) => {
   res.send({
     users,
   });
+});
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: "UserExistsError",
+        message: "A user by that username already exists",
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1w",
+      }
+    );
+
+    res.send({
+      message: "thank you for signing up",
+      token,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 usersRouter.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
